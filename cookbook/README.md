da6d73774d52b51d93ac23643aae9d4404ad5fc7
Kim Tore Jensen <kimtj@met.no>
2014-12-04 12:45:36 +0100

    Remove load balancer code, moved into wrapper cookbook.

--- cookbook/README.md
+++ cookbook/README.md
@@ -1,3 +1,3 @@
 # met-api-cookbook
 
-This cookbook provides a way to install an instance of the MET API, and also nginx functioning as a load balancer for all MET API nodes in the same Chef environment.
+This cookbook provides a way to install an instance of the Meteorological API.
--- cookbook/attributes/default.rb
+++ cookbook/attributes/default.rb
@@ -1,7 +1,6 @@
-# For the load balancer
-default['nginx']['default_site_enabled'] = false
-default['nginx']['init_style'] = 'upstart'
-
 # For met-api application servers
 default['java']['install_flavor'] = 'openjdk'
 default['java']['jdk_version'] = '7'
+
+# Application settings
+automatic['met-api']['port'] = 9000
